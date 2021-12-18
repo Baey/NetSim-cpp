cdad601411c8c2edaa1df8764d921ec3d667d170
Baey <blazejszargut@gmail.com>
2021-12-18 14:46:37 +0100

    Poprawki do deklaracji metod oraz definicje pop i push ale z błędami

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -16,25 +16,6 @@ enum PackageQueueType {
     LIFO, FIFO
 };
 
-/* Uważam, że klasa PackageQueue powinna dziedziczyć po IPackageQueue, bo jest ona implementacją interfejsu
- * jakim do obsługi kolejki. */
-class PackageQueue : IPackageQueue {
-public:
-    explicit PackageQueue(PackageQueueType package_queue_type) : package_queue_type_(std::move(queue_type)) {}
-
-    /* Ma być dostosowanie tego typu metod w zależności od tego z jakim typem kolejki mamy do czynienia (LIFO/FIFO),
-    * ale na ten moment nie wiem jak najsensowniej byłoby się tym zająć, więc implementuję tylko dla LIFO. */
-    void push(const Package &package) { package_queue_.emplace_back(package); }
-
-    const PackageQueue &const pop() { return package_queue_.pop_back(); }
-
-    PackageQueueType string_queue() const { return package_queue_type_; }
-
-private:
-    std::list<Package> package_queue_;
-    std::string package_queue_type_;
-};
-
 class IPackageStockpile {
 public:
     /* Miał być alias const_iterator zdefiniowany w jako publiczny w tej klasie.
@@ -63,11 +44,29 @@ private:
  * oraz metodę służącą do określenia typu kolejki, dlatego uważam że IPackageQueue dziedziczy po IPackageStockpile */
 class IPackageQueue : IPackageStockpile {
 public:
-    virtual Package pop() = 0;
-
-private:
+    virtual const Package pop() = 0;
 
     virtual PackageQueueType get_queue_type() = 0;
+};
+
+
+/* Uważam, że klasa PackageQueue powinna dziedziczyć po IPackageQueue, bo jest ona implementacją interfejsu
+ * jakim do obsługi kolejki. */
+class PackageQueue : IPackageQueue {
+public:
+    explicit PackageQueue(PackageQueueType package_queue_type) : package_queue_type_(package_queue_type) {}
+
+    /* Ma być dostosowanie tego typu metod w zależności od tego z jakim typem kolejki mamy do czynienia (LIFO/FIFO),
+    * ale na ten moment nie wiem jak najsensowniej byłoby się tym zająć, więc implementuję tylko dla LIFO. */
+    void push(const Package &package);
+
+    const Package pop();
+
+    PackageQueueType package_queue_type() const { return package_queue_type_; }
+
+private:
+    std::list<Package> package_queue_;
+    PackageQueueType package_queue_type_;
 };
 
 #endif //NETSIM_CPP_STORAGE_TYPES_HPP
--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -1,4 +1,24 @@
 //
 // Created by Błażej Szargut on 14/12/2021.
 //
+#include <list>
+#include <storage_types.hpp>
+
+/* Nie wiem co mu nie pasuje w typach w poniższych implementacjach */
+
+const Package PackageQueue::pop() {
+    if (package_queue_type_ == LIFO) {
+        return package_queue_.pop_back();
+    } else {
+        return package_queue_.pop_front();
+    }
+}
+
+void PackageQueue::push(const Package &package) {
+    if (package_queue_type_ == LIFO) {
+        package_queue_.emplace_back(package);
+    } else {
+        package_queue_.insert(package_queue_.front(), package);
+    }
+}
 
