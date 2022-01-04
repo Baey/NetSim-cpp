ac50aa208a6811eb67e7e51ba6ab324d535370ab
Błażej <blazejszargut@gmail.com>
2022-01-04 20:29:42 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/googletest-release-1.11.0/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/blaze/Desktop/Studia/ZPO/NetSim-cpp/googletest-release-1.11.0/googlemock
-# Build directory: C:/Users/blaze/Desktop/Studia/ZPO/NetSim-cpp/cmake-build-debug/googletest-release-1.11.0/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cmake-build-debug/googletest-release-1.11.0/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/blaze/Desktop/Studia/ZPO/NetSim-cpp/googletest-release-1.11.0/googletest
-# Build directory: C:/Users/blaze/Desktop/Studia/ZPO/NetSim-cpp/cmake-build-debug/googletest-release-1.11.0/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
