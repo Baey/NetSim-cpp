cc7a5d943cb7f2c872cb3abebb5926f3cd527b4d
Blazej Szargut <blazejszargut@gmail.com>
2022-01-04 17:27:08 +0100

    Framework googla do testów

---
+++ cmake-build-debug/NetSim_tests/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/blaze/Desktop/Studia/ZPO/NetSim-cpp/NetSim_tests/lib
+# Build directory: C:/Users/blaze/Desktop/Studia/ZPO/NetSim-cpp/cmake-build-debug/NetSim_tests/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/NetSim_tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/blaze/Desktop/Studia/ZPO/NetSim-cpp/NetSim_tests/lib/googletest
+# Build directory: C:/Users/blaze/Desktop/Studia/ZPO/NetSim-cpp/cmake-build-debug/NetSim_tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
